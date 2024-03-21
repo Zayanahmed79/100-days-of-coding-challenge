0a63b5ecfc07e37784fa5e026eed294cca78fd4d
Zayanahmed79 <160269067+Zayanahmed79@users.noreply.github.com>
2024-03-21 16:05:42 +0500

    Add files via upload

---
+++ Day 1/Qs.03.js
@@ -0,0 +1,4 @@
+var personname = "\t\n Sir Zia Khan \n\t";
+console.log(personname); //with whitespaces
+console.log(JSON.stringify(personname)); // without white spaces
+console.log(personname.trim()); // with stripping
---
+++ Day 1/Qs.03.ts
@@ -0,0 +1,7 @@
+let personname: string = "\t\n Sir Zia Khan \n\t";
+
+console.log(personname); //with whitespaces
+
+console.log(JSON.stringify(personname)); // without white spaces
+
+console.log(personname.trim()); // with stripping

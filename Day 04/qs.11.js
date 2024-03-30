ebb402ff99d55ea2cf51cfe8ecad486769975f97
Zayanahmed79 <160269067+Zayanahmed79@users.noreply.github.com>
2024-03-30 11:44:56 +0500

    Add files via upload

---
+++ Day 04/qs.11.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.names = void 0;
+var names = ["shafique", "sandeed", "fahad", "Nasir"];
+exports.names = names;
+// console.log(names[0]);
+// console.log(names[1]);
+// console.log(names[2]);
+// console.log(names[3]);
+// for (let a = 0; a < names.length; a++){
+//     console.log(names[a]);
+// }
+// for (let nam of names) {
+//     console.log(nam);
+// }
+names.forEach(function (val) { return console.log(val); });

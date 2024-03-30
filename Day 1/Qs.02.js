e19df06ef980d2cc5b387d58aa568aa818d6cc8c
Zayanahmed79 <160269067+Zayanahmed79@users.noreply.github.com>
2024-03-30 11:41:02 +0500

    Delete Day 1 directory

--- Day 1/Qs.02.js
+++
@@ -1,3 +0,0 @@
-var famous_person = "Albert Einstein";
-var message = " once said,\"A person who never made a mistake never tried anything new\"";
-console.log(famous_person + message);
--- Day 1/Qs.03.ts
+++
@@ -1,7 +0,0 @@
-let personname: string = "\t\n Sir Zia Khan \n\t";
-
-console.log(personname); //with whitespaces
-
-console.log(JSON.stringify(personname)); // without white spaces
-
-console.log(personname.trim()); // with stripping

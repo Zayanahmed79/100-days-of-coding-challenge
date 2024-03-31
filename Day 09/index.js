8ce5ec06b0602befa3e8810d0dc0bd36d556b956
Zayanahmed79 <160269067+Zayanahmed79@users.noreply.github.com>
2024-03-31 22:17:20 +0500

    Add files via upload

---
+++ Day 09/index.js
@@ -0,0 +1,24 @@
+//qs.no 25
+var alien_colour1 = "green";
+if (alien_colour1 === "green") {
+    console.log("Yes! Alien's colour is green");
+}
+//qs.no 26
+var alien_colour2 = "green";
+if (alien_colour2 === "gre") {
+    console.log("Yes! Alien's colour is green");
+}
+else {
+    console.log("No! thats not the colour of Alien");
+}
+//qs.no 27
+var alien_colour = "red";
+if (alien_colour == "green") {
+    console.log("Yes! Alien's colour is green, you just earned 5 points.");
+}
+else if (alien_colour == "red") {
+    console.log("Yes! Alien's colour is red, you just earned 10 points.");
+}
+else if (alien_colour == "yellow") {
+    console.log("Yes! Alien's colour is yellow, you just earned 15 points.");
+}
---
+++ Day 09/index.ts
@@ -0,0 +1,23 @@
+//qs.no 25
+let alien_colour1 = "green";
+if (alien_colour1 === "green") {
+  console.log("Yes! Alien's colour is green");
+}
+//qs.no 26
+let alien_colour2 = "green";
+
+if (alien_colour2 === "red") {
+  console.log("Yes! Alien's colour is green");
+} else {
+  console.log("No! thats not the colour of Alien");
+}
+
+//qs.no 27
+let alien_colour = "red";
+if (alien_colour == "green") {
+  console.log("Yes! Alien's colour is green, you just earned 5 points.");
+} else if (alien_colour == "red") {
+  console.log("Yes! Alien's colour is red, you just earned 10 points.");
+} else if (alien_colour == "yellow") {
+  console.log("Yes! Alien's colour is yellow, you just earned 15 points.");
+}

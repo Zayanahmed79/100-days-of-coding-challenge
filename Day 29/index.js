24adedf15bbb0ee594e1eee3b56bdde674978d88
Zayanahmed79 <160269067+Zayanahmed79@users.noreply.github.com>
2024-04-16 17:30:05 +0500

    Add files via upload

---
+++ Day 29/index.js
@@ -0,0 +1,15 @@
+//qs.no 85
+function locationofstring(word) {
+    return word.indexOf("a");
+}
+console.log(locationofstring("I build a code of differnt bread"));
+//qs.no 86
+function check(word) {
+    return word.includes("javascript");
+}
+console.log(check("I love typescript"));
+//qs.no  87
+function printsubstring(sentence) {
+    return sentence.substring(0, 10);
+}
+console.log((printsubstring("1,2,3,4,5,6,7,8,9,10,11")));
---
+++ Day 29/index.ts
@@ -0,0 +1,23 @@
+//qs.no 85
+
+function locationofstring(word : string){
+    return word.indexOf("a")
+}
+console.log(locationofstring("I build a code of differnt bread"));
+
+//qs.no 86
+
+function check(word : string):boolean{
+
+  return  word.includes("javascript")
+
+}
+console.log(check("I love typescript"))
+
+//qs.no  87
+
+function printsubstring(sentence : string):string{
+    return sentence.substring(0,10)
+}
+console.log((printsubstring("1,2,3,4,5,6,7,8,9,10,11")))
+

4d2cd5ba065264e1eb66261b2dd52b1029f439d1
Zayan Ahmed <160269067+Zayanahmed79@users.noreply.github.com>
2024-04-20 19:56:16 +0500

    Add files via upload

---
+++ Day 34/index.js
@@ -0,0 +1,17 @@
+//qs.no 100
+function squareroot(number) {
+    console.log(Math.sqrt(number));
+}
+squareroot(144);
+// qs.no.101
+function num1_10() {
+    var value = Math.floor((Math.random()) * 10) + 1;
+    console.log(value);
+}
+num1_10();
+//qs.no.102
+function abs() {
+    var aa = Math.abs(-5 - 5);
+    console.log(aa);
+}
+abs();
---
+++ Day 34/index.ts
@@ -0,0 +1,21 @@
+//qs.no 100
+function squareroot(number: number) {
+  console.log(Math.sqrt(number));
+}
+squareroot(144);
+
+// qs.no.101
+
+function num1_10() {
+  let value = Math.floor(Math.random() * 10) + 1;
+  console.log(value);
+}
+num1_10();
+
+//qs.no.102
+
+function abs() {
+  let aa = Math.abs(-5 - 5);
+  console.log(aa);
+}
+abs();

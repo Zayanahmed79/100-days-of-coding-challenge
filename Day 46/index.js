da2ec1dae35a6ff8b3041b9af967101ca16e3e9c
Zayan Ahmed <160269067+Zayanahmed79@users.noreply.github.com>
2024-05-09 08:43:44 -0700

    Add files via upload

---
+++ Day 46/index.js
@@ -0,0 +1,13 @@
+//qs.no 136
+for (var i = 0; i <= 5; i++) {
+    console.log("Iteration ".concat(i, ", i value: i"));
+}
+//qs.no 137
+console.log("---------------x------------------");
+try {
+    throw new Error("Something went wrong!");
+}
+catch (error) {
+    console.log("Fuck off!");
+}
+//qs.no 138
---
+++ Day 46/index.ts
@@ -0,0 +1,29 @@
+//qs.no 136
+
+for (let i = 0; i <= 5; i++){
+    console.log(`Iteration ${i}, i value: i`);
+    
+}
+
+//qs.no 137
+
+console.log("---------------x------------------");
+
+try{
+    throw new Error("Something went wrong!")
+}
+catch(error){
+
+    console.log(error.message)  
+}
+
+//qs.no 138
+
+// Sample code snippet for setting a breakpoint
+console.log("Before breakpoint");
+// Set a breakpoint on the following line using your browser's developer tools
+console.log(
+  "This line has a breakpoint set on it in the browser's developer tools"
+);
+console.log("After breakpoint");
+// To use breakpoints, open this code in your browser, open the developer tools, navigate to the Source tab, find this script, and click on the line number where you want to pause execution. When you reload or run your page, execution will pause at the breakpoint.

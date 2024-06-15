0b77e759d2431c7ba56b3ad65564d743ba0a2c81
Zayan Ahmed <160269067+Zayanahmed79@users.noreply.github.com>
2024-06-15 13:40:15 +0500

    Add files via upload

---
+++ Day 51/index.ts
@@ -0,0 +1,29 @@
+//        <<Top 10 Tips for the beginers>>
+
+//  1. Apply Your Theoretical Knowledge Practically (Practice, Practice, Practice…)
+
+//  How Practical is Important in Programming: 
+//  You become habitual of syntax and gain typing speed.
+//  You need to think a lot to solve problems in coding so it eventually develops problem-solving skills.
+//  It helps in getting the logic quickly to solve the problems.
+//  You become aware of common programming problems.
+
+//  2. Don’t Leave a Gap in Your Learning
+
+//  3. Don’t do Over Analysis of the Concept
+
+//  4. Read The Error Message Carefully
+
+//  5. Solve the MCQs Questions in Programming
+
+//  6. Code With Hands
+
+//  7. Read Other People’s Code
+
+//  8. Enhance Your Problem-Solving Skills
+
+//  9. Divide Things into Smaller Parts
+
+//  10. Find a Perfect Mentor
+
+//  <<<<Remember that learning the first programming language always requires special effort.>>>>
